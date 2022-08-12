8b9fd89fc4670304ad1ebb1ffe209702e7de5302
Vlad <mihusle@protonmail.com>
2022-08-12 22:39:54 +0400

    initial commit

---
+++ src/main/java/com/fakng/fakngnotif/FakngNotifApplication.java
@@ -0,0 +1,13 @@
+package com.fakng.fakngnotif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakngNotifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakngNotifApplication.class, args);
+    }
+
+}
